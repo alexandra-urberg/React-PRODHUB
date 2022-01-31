98c67068b1775ad14eaea2f9209e1dd4a2290418
Sasha Stepanova <chernishevskie@yandex.ru>
2022-01-31 12:33:50 +0200

    feat: added navigation component

---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Navigation() {
+    return(
+        <nav>
+            <ul>
+                <li>Главная </li>
+                <li>Кейсы</li>
+                <li>Процессы работы</li>
+                <li>Обо мне</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navigation;
