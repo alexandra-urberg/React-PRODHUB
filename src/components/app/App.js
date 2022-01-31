11140d4b15796744ee5fdfa923fc0e46c1369268
Sasha Stepanova <chernishevskie@yandex.ru>
2022-02-01 01:59:22 +0200

    feat: added App component + style

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import '../../index.css';
+import Header from '../header/Header';
+import Main from '../main/Main';
+import Footer from '../footer/Footer';
+import PopupNav from '../popupNav/PopupNav';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header/>
+      <Main/>
+      <Footer/>
+      <PopupNav/>
     </div>
   );
 }
