562703eb854cf5537a3885532321750c8ea8be64
Sasha Stepanova <chernishevskie@yandex.ru>
2022-01-31 12:33:32 +0200

    feat: added icons component

---
+++ src/components/icons/Icons.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Icons() {
+  return (
+    <div className="icons">
+      <img className="icon" src="#" alt="telegramm"></img>
+      <img className="icon" src="#" alt="instagram"></img>
+    </div>
+  );
+}
+
+export default Icons;
