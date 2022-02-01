8c363958863f1575c273162145436cd2d9bf45e6
Sasha Stepanova <chernishevskie@yandex.ru>
2022-02-01 02:01:32 +0200

    feat: added Container cmponent

--- src/components/container/Container.js
+++ src/components/container/Container.js
@@ -2,9 +2,9 @@ import React from "react";
 
 function Container() {
   return (
-    <div className="header_container">
-      <img className="header_icon" src="#" alt="main_icon"></img>
-      <h1 className="header_title">PROODHUB</h1>
+    <div className="container">
+      <div className="container__icon"/>
+      <h2 className="container__title">PRODHUB</h2>
     </div>
   );
 }
