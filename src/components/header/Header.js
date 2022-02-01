bb2f6d7e766f895e3edd29010e270a7221ece4ef
Sasha Stepanova <chernishevskie@yandex.ru>
2022-02-01 02:01:00 +0200

    feat: added header component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,16 +1,25 @@
 import React from "react";
+import MediaQuery from "react-responsive";
 import Container from "../container/Container";
 import Navigation from "../navigation/Navigation";
 import Icons from "../icons/Icons";
 
 function Header() {
-    return(
-        <header className="header">
-            <Container/>
-            <Navigation/>
-            <Icons/>
-        </header>
-    )
+  return (
+    <header className="header section">
+      <>
+        <MediaQuery minWidth={541}>
+          <Container />
+          <Navigation />
+          <Icons />
+        </MediaQuery>
+        <MediaQuery maxWidth={540}>
+          <Container />
+          <button className="header__menu-button"></button>
+        </MediaQuery>
+      </>
+    </header>
+  );
 }
 
 export default Header;
