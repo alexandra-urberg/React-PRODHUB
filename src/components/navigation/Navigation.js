bffb65f7122f3d796363252547ee3c57f86298ff
Sasha Stepanova <chernishevskie@yandex.ru>
2022-02-01 02:00:25 +0200

    feat:added Navigation component

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,13 +1,17 @@
 import React from "react";
+import MediaQuery from "react-responsive";
 
 function Navigation() {
     return(
-        <nav>
-            <ul>
-                <li>Главная </li>
-                <li>Кейсы</li>
-                <li>Процессы работы</li>
-                <li>Обо мне</li>
+        <nav className="navigation">
+            <MediaQuery maxWidth={540}>
+                <h2 className="navigation__title">Навигация</h2>
+            </MediaQuery>
+            <ul className="navigation__container">
+                <li className="navigation__links"><a className="navigation__link" href="@">Главная</a></li>
+                <li className="navigation__links"><a className="navigation__link" href="@">Кейсы</a></li>
+                <li className="navigation__links"><a className="navigation__link" href="@">Процессы работы</a></li>
+                <li className="navigation__links"><a className="navigation__link" href="@">Обо мне</a></li>
             </ul>
         </nav>
     )
