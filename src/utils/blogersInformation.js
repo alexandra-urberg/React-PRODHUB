c83702c7ea169231e86e05a85ee5325792993edf
Sasha Stepanova <chernishevskie@yandex.ru>
2022-02-02 04:23:07 +0200

    feat: added array

---
+++ src/utils/blogersInformation.js
@@ -0,0 +1,88 @@
+import AlexeyImg from '../images/unsplash_3TLl_97HNJo-2';
+import MariaImg from '../images/unsplash_3TLl_97HNJo-3';
+import EgorImg from '../images/unsplash_3TLl_97HNJo-4';
+import DariaImg from '../images/unsplash_3TLl_97HNJo-5';
+import none from '../index/unsplash_3TLl_97HNJo-6';
+import PolinaImg from '../images/unsplash_3TLl_97HNJo';
+
+const blogerInfo = [
+    {
+        image: PolinaImg,
+        name: "Полина Скирда",
+        insagram: "polina_skirda",
+        bussines: "Лайфстайл, домохозяйка",
+        subscribers: 43.7,
+        stories: 7,
+        salaru_before: "50 000 - 70 000",
+        salaru_after: "700 000",
+        review: "#",
+    },
+    {
+        image: AlexeyImg,
+        name: "Алексей Куреленок",
+        insagram: "kurelenokk",
+        bussines: "Предприниматель, общественный деятель",
+        subscribers: 78.1,
+        stories: 15,
+        salaru_before: "150 000 - 200 000",
+        salaru_after: "1 800 000",
+        review: "#",
+    },
+    {
+        image: MariaImg,
+        name: "Мария Калинина",
+        insagram: "kalininamariya",
+        bussines: "Контент для слабослышащих, Лайфстайл, домохозяйка",
+        subscribers: 54.1,
+        stories: 15,
+        salaru_before: "300 000 - 500 000",
+        salaru_after: "7 300 000",
+        review: "#",
+    },
+    {
+        image: EgorImg,
+        name: "Егор Лесной",
+        insagram: "egor_lesnoy",
+        bussines: "Лайфстайл, саморазвитие, здоровый образ жизни",
+        subscribers: 21.2,
+        stories: 6,
+        salaru_before: "10 000 - 20 000",
+        salaru_after: "299 000",
+        review: "#",
+    },
+    {
+        image: DariaImg,
+        name: "Дарья Астахова",
+        insagram: "dariaastakhova",
+        bussines: "Лайфстайл, бьюти",
+        subscribers: 39.9,
+        stories: 4,
+        salaru_before: "50 000 - 70 000",
+        salaru_after: "380 000",
+        review: "#",
+    },
+    {
+        image: none,
+        name: "Галя",
+        insagram: "-",
+        bussines: "-",
+        subscribers: "-",
+        stories: "-",
+        salaru_before: "-",
+        salaru_after: "-",
+        review: "#",
+    },
+    {
+        image: none,
+        name: "Галя",
+        insagram: "-",
+        bussines: "-",
+        subscribers: "-",
+        stories: "-",
+        salaru_before: "-",
+        salaru_after: "-",
+        review: "#",
+    },
+]
+
+export default blogerInfo;
