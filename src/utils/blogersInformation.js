d93ceb77a272fb2faa7f5f0b537761cfc88c8b66
Sasha Stepanova <chernishevskie@yandex.ru>
2022-02-03 21:04:24 +0200

    feat: added new position to array

--- src/utils/blogersInformation.js
+++ src/utils/blogersInformation.js
@@ -2,93 +2,84 @@ import AlexeyImg from '../images/unsplash_3TLl_97HNJo-2.jpg';
 import MariaImg from '../images/unsplash_3TLl_97HNJo-3.jpg';
 import EgorImg from '../images/unsplash_3TLl_97HNJo-4.jpg';
 import DariaImg from '../images/unsplash_3TLl_97HNJo-5.jpg';
-import none from '../images/unsplash_3TLl_97HNJo-6.jpg';
 import PolinaImg from '../images/unsplash_3TLl_97HNJo.jpg';
+import polinasVideo from '../videos/polina_skirda.MP4';
+import alexeysVideo from '../videos/kurelenokk.MP4';
+import mariasVideo from '../videos/kalininamariya.MP4';
+import egorsVideo from '../videos/egor_lesnoy.mp4';
+import dariasVideo from '../videos/dariaastakhova.MP4';
+
 
 const blogerInfo = [
     {
         image: PolinaImg,
         name: "Полина Скирда",
         insagram: "polina_skirda",
+        insta_link: "https://www.instagram.com/polina_skirda/",
         bussines: "Лайфстайл, домохозяйка",
         subscribers: 43.7,
         stories: 7,
         salary_before: "50 000 - 70 000",
         salary_after: "700 000",
         review: "#",
+        video: polinasVideo,
         id: 1,
     },
     {
         image: AlexeyImg,
         name: "Алексей Куреленок",
         insagram: "kurelenokk",
+        insta_link: "https://www.instagram.com/kurelenokk/",
         bussines: "Предприниматель, общественный деятель",
         subscribers: 78.1,
         stories: 15,
         salary_before: "150 000 - 200 000",
         salary_after: "1 800 000",
         review: "#",
+        video: alexeysVideo,
         id: 2,
     },
     {
         image: MariaImg,
         name: "Мария Калинина",
         insagram: "kalininamariya",
+        insta_link: "https://www.instagram.com/kalininamariya/",
         bussines: "Контент для слабослышащих, Лайфстайл, домохозяйка",
         subscribers: 54.1,
         stories: 15,
         salary_before: "300 000 - 500 000",
         salary_after: "7 300 000",
         review: "#",
+        video: mariasVideo,
         id: 3,
     },
     {
         image: EgorImg,
         name: "Егор Лесной",
         insagram: "egor_lesnoy",
+        insta_link: "https://www.instagram.com/egor_lesnoy/",
         bussines: "Лайфстайл, саморазвитие, здоровый образ жизни",
         subscribers: 21.2,
         stories: 6,
         salary_before: "10 000 - 20 000",
         salary_after: "299 000",
         review: "#",
+        video: egorsVideo,
         id: 4,
     },
     {
         image: DariaImg,
         name: "Дарья Астахова",
         insagram: "dariaastakhova",
+        insta_link: "https://www.instagram.com/dariaastakhova/",
         bussines: "Лайфстайл, бьюти",
         subscribers: 39.9,
         stories: 4,
         salary_before: "50 000 - 70 000",
         salary_after: "380 000",
         review: "#",
+        video: dariasVideo,
         id: 5,
-    },
-    {
-        image: none,
-        name: "Галя",
-        insagram: "-",
-        bussines: "-",
-        subscribers: "-",
-        stories: "-",
-        salary_before: "-",
-        salary_after: "-",
-        review: "#",
-        id: 6,
-    },
-    {
-        image: none,
-        name: "Галя",
-        insagram: "-",
-        bussines: "-",
-        subscribers: "-",
-        stories: "-",
-        salary_before: "-",
-        salary_after: "-",
-        review: "#",
-        id: 7,
     },
 ]
 
