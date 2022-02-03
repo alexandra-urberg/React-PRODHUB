eb8b86f881652246427137ac2255bf674ff471ee
Sasha Stepanova <chernishevskie@yandex.ru>
2022-02-04 01:09:52 +0200

    style: changed  card layout

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -19,20 +19,20 @@ function Card(card) {
         <div className="card__user">
           <img className="card__image" src={card.image} alt={card.name} />
           <div className="card__user-personality">
-            <p className="card__name gold">{card.name}</p>
+            <p className={`card__name gold ${card.id === 2 ? "card__name-s" : "" }`}>{card.name}</p>
             <div className="card__user-information">
               <img
                 className="card__icon"
                 src={instaColor}
                 alt="instagram_icon"
               />
-              <p className="card__insta-name">{card.insagram}</p>
+              <a className="card__insta-name" href={card.insta_link}>{card.insagram}</a>
             </div>
           </div>
         </div>
-        <h3 className="card__area">{card.bussines}</h3>
-        <ul className="card__box">
-          <li className="card__user-info">
+        <h3 className={`card__area ${card.id === 2 ? "card__name-sp" : "" }`}>{card.bussines}</h3>
+        <ul className={`card__box ${card.id === 1 ? "card__user-mg" : "" }`}>
+          <li className={`card__user-info ${card.id === 5 ? "card__user-mg" : "" }`}>
             <p className="card__insta-name">Подписчиков:</p>
             <p className="card__amount">{card.subscribers}к</p>
           </li>
@@ -45,20 +45,20 @@ function Card(card) {
             <p className="card__amount">{card.salary_before}&#8381;</p>
           </li>
         </ul>
-        <div className="border">
+        <div className="border border__width">
           <div className="card__amount-new">
             <h4 className="card__insta-name">Доход после:</h4>
             <p className="card__money gold">{card.salary_after}&#8381;</p>
           </div>
         </div>
         <button
-          className="card__button card__button-text"
+          className="card__button card__button-text card__button-wdt"
           onClick={handleOpenVideo}
         >
           Видеоотзыв
         </button>
       </div>
-      <PopupReview onClose={handleCloseVideo} isOpen={isPopupVideo} />
+      <PopupReview onClose={handleCloseVideo} isOpen={isPopupVideo} video={card.video}/>
     </li>
   );
 }
