108208c424e5444c562a7da74d54580316113b52
Sasha Stepanova <chernishevskie@yandex.ru>
2022-02-06 03:08:33 +0200

    refactor: changed classes names

--- src/components/popupReview/PopupReview.js
+++ src/components/popupReview/PopupReview.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import ReactPlayer from "react-player";
 import Controler from "../controler/Controler";
 
-function PopupReview({ isOpen, onClose, video }) {
+function PopupReview({ isOpen, onClose, video, videoId }) {
   const [isPlay, setIsPlay] = useState({ playing: false });
 
   const { playing } = isPlay;
@@ -11,21 +11,21 @@ function PopupReview({ isOpen, onClose, video }) {
     setIsPlay({ ...isPlay, playing: !isPlay.playing });
   };
 
-  console.log(isPlay.playing);
+  //console.log(isPlay.playing);
+  //console.log({videoId})
   return (
-    <div className={`popupNav__close ${isOpen ? "popupReview__open" : ""}`}>
+    <div className={`popupNav ${isOpen ? "popupNav__open" : ""}`}>
       <div className="popupReview">
         <div className="popupReview__container">
           <p className="popupReview__text">Видеоотзыв</p>
           <button className="popupNav__btn" onClick={onClose}></button>
         </div>
-        <div className="popupReview__img">
+        <div className="popupReview__wrapper">
           <ReactPlayer
-            className="popupReview__img"
             url={`${isOpen ? video : "none"}`}
             pip
             width="100%"
-            height="20%"
+            height="100%"
             playing={playing}
             controls={false}
           />
